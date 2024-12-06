b7c212903efcfcd66594a429b6f931f8c667f445
Carlos Campaña <carlos.campana.d@gmail.com>
2024-12-06 13:29:31 -0300

    ASD

--- API/urls.py
+++ API/urls.py
@@ -3,7 +3,6 @@ from django.urls import path
 from django.contrib import admin
 
 urlpatterns =[
-    path('admin/', admin.site.urls),
     path('producto/',views.ProductoList.as_view()),
     path('producto/<int:pk>',views.ProductoDetalle.as_view()),
     path('carrito/',views.CarritoList.as_view()),
