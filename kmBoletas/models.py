24b313076bc95a2b0eb4079ee4da31dcfe591970
Alumnos La Granja <alumnoslgr@inacap.cl>
2024-12-10 18:44:08 -0300

    agregar logica renovada boleta

--- kmBoletas/models.py
+++ kmBoletas/models.py
@@ -5,11 +5,23 @@ from django.contrib.auth.models import User
 from django.core.exceptions import ValidationError
 from django.db.models import Sum
 
+# class Boleta(models.Model):
+#     numero_boleta = models.CharField(max_length=20, unique=True)
+#     fecha_emision = models.DateTimeField(auto_now_add=True)
+#     cliente = models.ForeignKey(User, on_delete=models.CASCADE, related_name='boletas')
+#     total = models.PositiveIntegerField()
+
+#     def calcular_total(self):
+#         self.total = sum(detalle.subtotal for detalle in self.detalles.all())
+#         self.save()
+
+#     def __str__(self):
+#         return f"Boleta #{self.numero_boleta} - {self.cliente.username}"
 class Boleta(models.Model):
     numero_boleta = models.CharField(max_length=20, unique=True)
     fecha_emision = models.DateTimeField(auto_now_add=True)
     cliente = models.ForeignKey(User, on_delete=models.CASCADE, related_name='boletas')
-    total = models.PositiveIntegerField()
+    total = models.PositiveIntegerField(null=True, blank=True)  # Permite que 'total' sea nulo
 
     def calcular_total(self):
         self.total = sum(detalle.subtotal for detalle in self.detalles.all())
